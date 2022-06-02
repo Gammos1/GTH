34742f324f2b0649d24e8722fd4740dbb2af11b4
Gammos <79102967+Gammos1@users.noreply.github.com>
2022-06-02 09:06:10 +0200

    Speed

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var lastScore = 0;
 var stuff = 0;
 
 function cursorMove(event){
-    speed = 1;
+    speed = 3;
     var left = event.clientX - 25;
     if(left > 0 && left < 280) {
         character.style.left = left + "px";
