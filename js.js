ba013cdd9ae51cbf37d943f08214bf6a92baae83
Gammos <gammos@gmx.de>
2023-04-11 23:16:24 +0200

    te

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var money = parseInt(localStorage.getItem('money'));
-if(money != 0) {document.getElementById("money").innerHTML = money;} else {money = 0;}
+document.getElementById("money").innerHTML = money;
 
 const fileInput = document.getElementById("input-file");
 fileInput.addEventListener("input", async (evt) => {
