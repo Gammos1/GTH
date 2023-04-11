40a0055e4480e21bac911babb311394fec4e5897
Gammos <gammos@gmx.de>
2023-04-11 22:25:44 +0200

    fsd

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 var money = localStorage.getItem('money');
 if(money != 0) {document.getElementById("money").innerHTML = money;} else {money = 0;}
 
-
 const fileInput = document.getElementById("input-file");
 fileInput.addEventListener("input", async (evt) => {
   const [ file ] = fileInput.files;
